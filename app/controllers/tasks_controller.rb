dbd12d7562ccb4fe959a3ebd38705c75626ba447
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-10 23:01:32 +0200

    refactor controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,14 @@
 class TasksController < ApplicationController
+  # refactor
+  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # refactor - not dry
+    # @task = Task.find(params[:id])
   end
 
   def new
@@ -21,11 +25,13 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # refactor - not dry
+    # @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    # refactor not dry
+    # @task = Task.find(params[:id])
     if @task.update(task_params)
       redirect_to tasks_path
     else
@@ -34,14 +40,20 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    # refactor not dry
+    # @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
 
   private
+  # refactor method
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
+    # strong params
     params.require(:task).permit(:title, :details, :completed)
   end
 end
